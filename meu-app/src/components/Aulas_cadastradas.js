1ce7301bb4876bcf0856448a22dbcbc21764507c
gavasarcenio-crypto <ga.vas.arcenio@gmail.com>
2026-05-18 15:03:42 -0300

    add: components

--- meu-app/src/components/Aulas_cadastradas.js
+++
@@ -1,5 +0,0 @@
-
-/* 
-nesse componente vai mostrar as aulas padrão, matéria, horário e dia da aula
-
-*/
--- meu-app/src/components/LeftBar.js
+++
@@ -1,6 +0,0 @@
-
-/* 
-essa barra deve ser colocada em toda a tela usada para 
-navegar entre as 5 telas com botões 
-e mostrar também a tela em que o usuario está agora. 
-*/
