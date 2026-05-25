c4eeddbffc678c115ec106885e1111b0b959f291
guilhermelacerdaoliveira <guilacerda1503@gmail.com>
2026-05-25 13:48:00 -0300

    tela inicial

--- meu-app/src/App.jsx
+++ meu-app/src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
-  return (
-    <div>
-      <h1>Lição de PW</h1>
-      <p>apredendo a usar o vite</p>
-      <br></br>
-      <img 
-        src={viteLogo} 
-        alt="Logo do Vite" 
-        width="150"
-      />
-    </div>
-  )
+  return <HomeScreen />;
 }
 
-export default App
+export default App;
--- meu-app/src/screens/HomeScreen.js
+++
@@ -1,8 +0,0 @@
-
-/* 
-Na tela de inicio vão aparecer informações para criar um novo
-horario caso não exista, e ai vai ter como criar ou importar
-um horario, caso ja tenha um horario inserido então vai mostrar 
-as aulas do dia, a aula de agora, a proxima, os feriados dos proximos dias 
-claro, no titulo vai ter o nome do site
-*/
